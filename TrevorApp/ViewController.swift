f2d323256c74ee6c56ff6c8fc8d796f5d39dfddc
dk <none@gmail.com>
2019-06-25 17:48:12 -0700

    Initial Commit

---
+++ TrevorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrevorApp
+//
+//  Created by airMac on 2019-06-25.
+//  Copyright © 2019 Desmand King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
